68b6f067f488b1d3aed70e05d9c095a2b667ff73
Shantanu Dey Anik <40882871+Shantanu2645@users.noreply.github.com>
2023-02-26 15:08:45 +0600

    Update cyber_news_bot.py

--- cyber_news_bot.py
+++ cyber_news_bot.py
@@ -4,8 +4,6 @@ import feedparser
 import requests
 import time
 
-
-
 bleeping_url = "https://www.bleepingcomputer.com/feed/"
 packetstorm_url  = 'https://rss.packetstormsecurity.com/'
 theHNews_url = "https://feeds.feedburner.com/TheHackersNews"
@@ -20,27 +18,21 @@ try:
     hackaday_feed = feedparser.parse(hackaday_url).entries[0]
 
 except IndexError:
-
     bleeping_feed = {}
     thehackernews_feed  = {}
     nakedsec_feed    = {}
     packetstorm_feed = {}
     hackaday_feed = {}
 
-
-
 def bleeping():
     global bleeping_feed
     global bleeping_url
 
-    if not bleeping_feed:
-        bleeping_feed = feedparser.parse(bleeping_url).entries[0]
+    try:
+        if not bleeping_feed:
+            bleeping_feed = feedparser.parse(bleeping_url).entries[0]
 
-    else:
- 
-        try:
-            
-
+        else:
             feed = feedparser.parse(bleeping_url)
             latest_feed_entry = feed.entries[0]
             if latest_feed_entry['title'] != bleeping_feed['title']:
@@ -50,26 +42,22 @@ def bleeping():
                         "username": "BleepingNews",
                         "avatar_url": "https://w7.pngwing.com/pngs/507/494/png-transparent-adwcleaner-potentially-unwanted-program-adware-browser-hijacking-computer-software-computer-computer-logo-computer-program-thumbnail.png",
                         "attachments": []
-                        }
-                
-
+                       }
                 requests.post(webhook_url,json=news)
             bleeping_feed = latest_feed_entry
             time.sleep(interval)
-        except IndexError:
-            pass
+    except IndexError:
+        pass
 
 def thehackernews():
-
     global thehackernews_feed
     global theHNews_url
-    if not thehackernews_feed:
-        thehackernews_feed  = feedparser.parse(theHNews_url).entries[0]
-    
-    else:
-        try:     
+    try:
+        if not thehackernews_feed:
+            thehackernews_feed  = feedparser.parse(theHNews_url).entries[0]
+        else:
             feed = feedparser.parse(theHNews_url)
-            latest_feed = feed.entries[0]   
+            latest_feed = feed.entries[0]
 
             if latest_feed['title'] != thehackernews_feed['title']:
                 news = {
@@ -81,19 +69,16 @@ def thehackernews():
                 requests.post(webhook_url,json=news)
                 thehackernews_feed = latest_feed
             time.sleep(interval)
-        except IndexError:
-            pass
+    except IndexError:
+        pass
 
 def nakedsecurity():
-
     global nakedsec_feed
     global nakedsec_url
-
-    if not nakedsec_feed:
-        nakedsec_feed = feedparser.parse(nakedsec_url).entries[0]
-
-    else:   
-        try:
+    try:
+        if not nakedsec_feed:
+            nakedsec_feed = feedparser.parse(nakedsec_url).entries[0]
+        else:
             feed = feedparser.parse(nakedsec_url)
             latest_feed = feed.entries[0]
             if latest_feed != nakedsec_feed :
@@ -103,24 +88,19 @@ def nakedsecurity():
                     "avatar_url": "https://media.licdn.com/dms/image/C4D0BAQG7JPzNAuTXEA/company-logo_200_200/0/1579611356895?e=2147483647&v=beta&t=f8GxbDi1dQIjgSAx_QverApaq8lPIEM8IzZAjCOCXl8",
                     "attachments": []
                     }
-
                 requests.post(webhook_url,json=news)
             nakedsec_feed = latest_feed
             time.sleep(interval)
-        except IndexError:
-            pass
+    except IndexError:
+        pass
 
 def packetstorm():
     global packetstorm_feed
     global packetstorm_url
-
-    if not packetstorm_feed:
-        packetstorm_feed    = feedparser.parse(packetstorm_url).entries[0]
-
-#aka packetstromsecurity
-    else:
-        try:
-
+    try:
+        if not packetstorm_feed:
+            packetstorm_feed    = feedparser.parse(packetstorm_url).entries[0]
+        else:
             feed = feedparser.parse(packetstorm_url)
             latest_news_feed = feed.entries[0]
             if latest_news_feed != packetstorm_feed:
@@ -131,25 +111,21 @@ def packetstorm():
                     "avatar_url": "https://upload.wikimedia.org/wikipedia/commons/5/55/Hero_Alom.png",
                     "attachments": []
                 }
-
                 requests.post(webhook_url,json=news)
                 packetstorm_feed = latest_news_feed
             time.sleep(interval)
-        except IndexError:
-            pass
+    except IndexError:
+        pass
 
 def hackaday():
-    
     global hackaday_feed
     global hackaday_url
-
-    if not hackaday_feed:
-        hackaday_feed = feedparser.parse(hackaday_url).entries[0]
-    else:
-        try:
+    try:
+        if not hackaday_feed:
+            hackaday_feed = feedparser.parse(hackaday_url).entries[0]
+        else:
             feed = feedparser.parse(hackaday_url)
             hackaday_latest_feed = feed.entries[0]
-
             if hackaday_latest_feed['title'] != hackaday_feed['title']:
                 news = {
                         "content": hackaday_latest_feed['title']+"\n"+hackaday_latest_feed['link'],
@@ -158,24 +134,18 @@ def hackaday():
                         "attachments": []
                         }
                 requests.post(webhook_url, json=news)
-
                 hackaday_feed = hackaday_latest_feed
-            
             time.sleep(interval)
 
-        except IndexError:
-            pass
+    except IndexError:
+        pass
 
-
-
-
-webhook_url = "https://discord_webhook_url_here"
+webhook_url = "https://discordapp.com/api/webhooks/1072062492731260968/nOIZYgVMTMbShuwwV9IukiQlelCLaxMkSMA4CxBwkX_Ywve3AMWa6J9fTOB29RnTB9FC"
 interval = 12
 
-while True:    
+while True:
     bleeping()
     thehackernews()
     nakedsecurity()
     packetstorm()
     hackaday()
-
